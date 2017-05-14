353160eeb9241ff4ed373f899a2b8871354b1efc
Jamen Marzonie <jamenmarz@gmail.com>
2017-05-14 00:06:24 -0500

    Fix var

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = imux
 
 function imux (streams) {
   var input = splitter(streams)
-  var transform = input[0]
+  var sink = input[0]
   var sources = input[1]
   var rest = input[2]
   var p = pushable(true)
@@ -19,9 +19,7 @@ function imux (streams) {
     channels[name] = {
       source: sources[name],
       sink: drain(p.push, function (err) {
-        if (err || !--pending) {
-          return p.end(err) 
-        }
+        if (err || !--pending) p.end(err) 
       })
     }
   }
